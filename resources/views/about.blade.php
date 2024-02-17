374c4ded5edc581eb4907c7b0eaa529719bbd09c
Zhanel Talip <janel.talip04@mail.ru>
2024-02-18 01:20:37 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.standard')
+
+@section('title')Про нас@endsection
+
+@section('content')
+    <h1>Про нас</h1>
+@endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.standard')
+
+@section('title')Главная страница@endsection
+
+@section('content')
+<h1>Главная страница</h1>
+@endsection
+
+
+
+
